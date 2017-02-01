173e6b439697b9f18cbb1e2667c806e0697b8174
sourceperl <loic.celine@free.fr>
2017-02-01 14:15:09 +0100

    improve RTU monitor

--- mb_rtu_mon.py
+++ mb_rtu_mon.py
@@ -5,33 +5,36 @@
 # modbus/RTU python monitor (check serial frame between devices)
 
 # format modbus RTU serial frame receive from a serial port to console stdout
-# check with python2.7 and python3
+# check with python2.7 and python3, and module serial 2.6
 
 # synoptic :
 #
-#   RS-485 bus -> FTDI USB cable ->  /dev/ttyUSBx -> this_script -> CSV modbus
-#   RS-422                                                            decode
+#   RS-485 bus -> FTDI USB cable -> /dev/ttyUSBx -> this_script -> CSV modbus
+#   RS-422                                                           decode
 #   RS-232
 
 # CSV out sample :
 # DATE=2015-11-05T17:31:27.977;ERR=NO;FRAME=01-03-50-00-00-04-55-09;SLAVE=1;
 
 # use it like this :
+#   - help :
+#       $ ./mb_rtu_mon.py -h
 #   - see frame only for slave 17 :
 #       $ ./mb_rtu_mon.py | grep SLAVE=0x11
 #   - store all traffic on a file
 #       $ ./mb_rtu_mon.py > modbus.log
 
+
+import argparse
 import sys
 import serial
 import struct
+import time
 from datetime import datetime
 
 # some const 
-DEVICE = '/dev/ttyUSB0'
-BAUDRATE = 9600
-TIMEOUT = 0.0037 # 3.7ms for 9600 bauds
 SEP = ";"
+
 
 def frame2crc(frame):
     """Compute modbus CRC16 (for RTU mode)
@@ -52,54 +55,72 @@ def frame2crc(frame):
                 crc ^= 0xA001
     return crc
 
-# init serial object
-ser = serial.Serial(DEVICE, BAUDRATE)
 
-# serial timeout = modbus end of frame (3.5 * byte tx time)
-ser.timeout = TIMEOUT
+if __name__ == '__main__':
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--device', type=str, default='/dev/ttyUSB0',
+                        help='serial device (default is /dev/ttyUSB0)')
+    parser.add_argument('-b', '--baudrate', type=int, default=9600,
+                        help='serial baudrate (default is 9600)')
+    args = parser.parse_args()
 
-while True:
-    # maximum size of modbus RTU frame is 256 bytes
-    frame = ser.read(256)
-    # for null size (no data)
-    if not frame:
-      continue
-    # init vars
-    err_str = "NO"
-    slave_id = 0
-    f_code = 0
-    e_code = 0
-    # add date and time
-    now = datetime.now()
-    date = now.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
-    # format modbus frame as txt string
-    txt_frame = ''
-    for item in bytearray(frame):
-        if txt_frame:
-            txt_frame += "-"
-        txt_frame += "%02x" % item
-    # check short frame
-    if len(frame) >= 5:
-        # check CRC
-        r_crc = struct.unpack("<H", frame[-2:])[0]
-        c_crc = frame2crc(frame[:-2])
-        crc_ok = (r_crc == c_crc)
-        if not crc_ok:
-            err_str = "BAD_CRC"
-        else :
-            # slave ID
-            slave_id = struct.unpack("B", frame[0:1])[0]
-            # function code
-            f_code = struct.unpack("B", frame[1:2])[0]
-            # except code
-            if f_code > 0x80:
-                err_str = "EXCEPT_FRAME"
-                e_code = struct.unpack("B", frame[2:3])[0]
-    else:
-        err_str = "SHORT_FRAME"
-    # out result
-    csv_line = ("DATE={0}"+SEP+"ERR={1}"+SEP+"FRAME={2}"+SEP+"SLAVE={3}"+SEP).format(date, err_str, txt_frame, slave_id)
-    print(csv_line)
-    sys.stdout.flush()
-       
-ser.close()
+    # init serial object
+    ser = serial.Serial(args.device, args.baudrate)
+
+    # serial timeout = modbus end of frame (3.5 * byte tx time + 10% margin)
+    ser.timeout = (1.0 / args.baudrate) * 11 * 3.5 * 1.10
+
+    # flush all
+    ser.flushInput()
+
+    # first read (used for sync to frame, avoid bad CRC)
+    ser.read(256)
+
+    # main loop
+    while True:
+        # maximum size of modbus RTU frame is 256 bytes
+        frame = ser.read(256)
+        # for null size (no data)
+        if not frame:
+            continue
+        # init vars
+        err_str = "NO"
+        slave_id = 0
+        f_code = 0
+        e_code = 0
+        # add date and time
+        now = datetime.now()
+        date = now.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
+        # format modbus frame as txt string
+        txt_frame = ''
+        for item in bytearray(frame):
+            if txt_frame:
+                txt_frame += "-"
+            txt_frame += "%02x" % item
+        # check short frame
+        if len(frame) >= 5:
+            # check CRC
+            r_crc = struct.unpack("<H", frame[-2:])[0]
+            c_crc = frame2crc(frame[:-2])
+            crc_ok = (r_crc == c_crc)
+            if not crc_ok:
+                err_str = "BAD_CRC"
+            else:
+                # slave ID
+                slave_id = struct.unpack("B", frame[0:1])[0]
+                # function code
+                f_code = struct.unpack("B", frame[1:2])[0]
+                # except code
+                if f_code > 0x80:
+                    err_str = "EXCEPT_FRAME"
+                    e_code = struct.unpack("B", frame[2:3])[0]
+        else:
+            err_str = "SHORT_FRAME"
+        # send result to stdout
+        items = ['DATE=%s' % date, 'ERR=%s' % err_str, 'FRAME=%s' % txt_frame, 'SLAVE=%s' % slave_id]
+        csv_line = SEP.join(items)
+        print(csv_line)
+        sys.stdout.flush()
+
+    ser.close()
