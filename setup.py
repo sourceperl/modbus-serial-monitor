fadfc01ed1e06de2c426d99c957208e099a95ff5
sourceperl <loic.celine@free.fr>
2017-02-03 11:11:45 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='modbus-serial-monitor',
+    version='0.0.1',
+    description='A set of tools for modbus diagnostic',
+    long_description='',
+    author='Loic Lefebvre',
+    author_email='loic.celine@free.fr',
+    license='MIT',
+    url='https://github.com/sourceperl/modbus-serial-monitor',
+    platforms='any',
+    install_requires=required,
+    scripts=[
+        'scan_modbus_serial',
+    ],
+)
